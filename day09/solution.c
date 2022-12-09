7c62dc43154cc181c48cabf8c9d216c64eaa2b77
Michal Brzozowski <michal.brzozowski@hotmail.com>
2022-12-09 09:36:00 +0100

    code for second part day09 + cleanup

--- day09/solution.c
+++ day09/solution.c
@@ -1,8 +1,10 @@
+#include <string.h>
+
 #include "..\utilities.h"
 
 #define input_file "input"
 
-#define grid_size 4096
+#define grid_size 512
 static u32 grid[grid_size][grid_size];
 
 typedef struct point {
@@ -12,10 +14,10 @@ typedef struct point {
 
 point point_subtract(point first, point second)
 {
-    point result = { 0 };
-    result.x = first.x - second.x;
-    result.y = first.y - second.y;
-    return result;
+    return (point) {
+        .x = first.x - second.x,
+        .y = first.y - second.y
+    };
 }
 
 point move_tail(point head, point tail)
@@ -31,55 +33,60 @@ point move_tail(point head, point tail)
     return move;
 }
 
-int part_one()
+int move_rope(int rope_length)
 {
+    memset(grid, 0, sizeof(grid));
     int answer = 0;
     char** input = aquire_input(input_file);
 
-    // offset so we dont go out of bounds on the grid.
-    point head = { .x = grid_size / 2, .y = grid_size / 2 };
-    point tail = { .x = grid_size / 2, .y = grid_size / 2 };
+    point* rope = null_ptr;
+    for (int i = 0; i < 10; ++i) {
+        list_push(rope, (point) { .x = grid_size / 2, .y = grid_size / 2 });
+    }
 
     char direction;
     int steps;
     for (int i = 0; i < list_length(input); ++i) {
         sscanf_s(input[i], "%c %d", &direction, 1, &steps);
-
         for (int j = 0; j < steps; ++j) {
+            point *head = &rope[0];
             switch (direction)
             {
-                case 'U': { head.y += 1; } break;
-                case 'D': { head.y -= 1; } break;
-                case 'L': { head.x -= 1; } break;
-                case 'R': { head.x += 1; } break;
+                case 'U': { head->y += 1; } break;
+                case 'D': { head->y -= 1; } break;
+                case 'L': { head->x -= 1; } break;
+                case 'R': { head->x += 1; } break;
             }
+            for (int k = 1; k < 10; ++k) {
+                head = &rope[k - 1];
+                point *tail = &rope[k];
 
-            point tail_step = move_tail(head, tail);
-            tail.x += tail_step.x;
-            tail.y += tail_step.y;
-
-            grid[tail.y][tail.x]++;
+                point tail_step = move_tail(*head, *tail);
+                tail->x += tail_step.x;
+                tail->y += tail_step.y;
+            }
+            grid[rope[rope_length - 1].y][rope[rope_length - 1].x]++;
         }
     }
-
     for (int i = 0; i < grid_size; ++i) {
         for (int j = 0; j < grid_size; ++j) {
             if (grid[i][j] > 0) 
                 answer++;
         }
     }
-
+    list_free(rope);
     release_input(input);
     return answer;
 }
 
+int part_one()
+{
+    return move_rope(2);
+}
+
 int part_two()
 {
-    int answer = 0;
-    char** input = aquire_input(input_file);
-
-    release_input(input);
-    return answer;
+    return move_rope(10);
 }
 
 void main()
